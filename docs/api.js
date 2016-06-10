9ed335c2f6b92641c2a90e160cdeecdb4991bef3
Chris Ball <chris@echobind.com>
2016-06-10 17:41:13 -0400

    Released v0.2.2

---
+++ docs/api.js
@@ -0,0 +1,24 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "HtmlTag",
+        "LinksWithFollower"
+    ],
+    "modules": [
+        "Components",
+        "Home"
+    ],
+    "allModules": [
+        {
+            "displayName": "Components",
+            "name": "Components"
+        },
+        {
+            "displayName": "Home",
+            "name": "Home",
+            "description": "## Features\nProvides a component that renders a set of links with a follower line underneath. The follower will animate to the position of the active link, and grow or shrink in size if needed. If no active links are found, the follower will hide.\n\nThis pattern is commonly used for navigation:\n\n![twitter](https://raw.githubusercontent.com/echobind/ember-links-with-follower/master/public/assets/images/twitter.png)\n\n(from Twitter app)\n<p>&nbsp;</p>\n\n![linkedin](https://raw.githubusercontent.com/echobind/ember-links-with-follower/master/public/assets/images/linkedin.png)\n\n(from LinkedIn app)\n\n---\n\nCheckout the [LinksWithFollower](../classes/LinksWithFollower.html) component!\n\n<div style=\"position:absolute; bottom: 20px; right: 20px;\">\n(<a href=\"https://github.com/offirgolan/yuidoc-ember-theme\">yuidoc-ember-theme</a> by Offir Golan)\n</div>"
+        }
+    ],
+    "elements": []
+} };
+});
